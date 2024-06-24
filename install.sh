1174f39a1a6e8ce39ebb4a1c5dcf65700b97d4c4
Inozpavel <Inozpavel@mail,ru>
2024-06-25 00:27:55 +0300

    Add packages update, prepare for packages install

--- install.sh
+++ install.sh
@@ -6,23 +6,56 @@ ALIASES=(
 "cls=\"clear\""
 )
 
+PACKAGES=(
+"btop"
+#"nano"
+)
+
 TERMINAL_FILE="$HOME/.zshrc"
 
-addAliases(){
-  for als in "${ALIASES[@]}"
-  do
-    line="alias ${als}"
-    if ! grep -q "$line" "$TERMINAL_FILE"
+main() {
+  echo "Installation starting"
+
+  add_aliases
+  echo
+  update_sources_and_installed_packages
+  echo
+  add_packages
+
+  echo "Installation success"
+}
+
+add_aliases() {
+  for als in "${ALIASES[@]}"; do
+    local _line="alias ${als}"
+    if ! grep -q "$_line" "$TERMINAL_FILE"
     then
       echo "Adding alias ${als}"
-      echo "Line $line"
-      echo "$line" >> "$TERMINAL_FILE"
+      echo "Line $_line"
+      echo "$_line" >> "$TERMINAL_FILE"
     fi
   done
 }
 
-echo "Installation starting"
+update_sources_and_installed_packages() {
+  echo "Updating packages..."
+  sudo pacman -Suy
+  echo "Package update complete"
+}
 
-addAliases
+add_packages() {
+    local _packages_line
 
-echo "Installation success"
+    echo "Installing required packages..."
+    _packages_line=$(printf " %s" "${PACKAGES[@]}")
+    echo "$_packages_line"
+
+#    yes | sudo pacman -S "${_packages_line}"
+    sudo pacman -S "${_packages_line}"
+
+    echo "Installing required packages completed"
+}
+
+
+main "$@"|| exit 1
+
