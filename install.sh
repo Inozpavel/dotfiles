766c1d648f8a620fad4e15ee96d299dbf9534f9c
Inozpavel <Inozpavel@mail,ru>
2024-06-27 00:10:55 +0300

    Revert sudo requirement

--- install.sh
+++ install.sh
@@ -38,12 +38,14 @@ add_packages() {
 }
 
 install_oh_my_zsh(){
-  сp "$(dirname $"0")"/.zshrc "$HOME/.zshrc"
-  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+  echo "Directory: $(dirname "$0")/.zshrc"
+  echo "$HOME"
+  сp "$(dirname "$0")"/.zshrc "$HOME/.zshrc"
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --keep-zshrc --unattended
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
   git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-  chsh -s "$(which zsh)"
+  chsh -s "$(which zsh)" "${SUDO_USER:-"$USER"}"
 }
 
 
