9405c369ec900e7aa1e43bf06c8d0ace442c94fd
inozpavel <inozpavel@mail.ru>
2024-07-08 23:15:27 +0300

    Disable warn_about_missing_glyphs

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 --config.window_background_opacity = 0.9
 config.font = wezterm.font('JetBrainsMono')
 config.font_size = 15
+config.warn_about_missing_glyphs = false
 
 --config.color_scheme = 'Sakura'
 config.color_scheme = 'Slate'
