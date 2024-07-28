84f4e09e9e8254f3fa5fe18a0f6e51110bf62eb4
inozpavel <inozpavel@mail.ru>
2024-07-29 01:23:42 +0300

    Add scroll font change for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 wezterm.add_to_config_reload_watch_list('/home/inozpavel/.cache/wal/wezterm-wal.toml')
 
@@ -40,5 +41,22 @@ config.default_prog = { '/usr/bin/zsh' }
 
 config.initial_cols = 130
 config.initial_rows = 35
+
+config.mouse_bindings = {
+  -- Scrolling up while holding CTRL increases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = act.IncreaseFontSize,
+  },
+
+  -- Scrolling down while holding CTRL decreases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = act.DecreaseFontSize,
+  },
+}
+
 
 return config
