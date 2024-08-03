d538393df350bdc011af6c4d51f55a5734c170f8
inozpavel <inozpavel@mail.ru>
2024-08-03 18:58:56 +0300

    Add rofi image show

--- scripts/cliphist.sh
+++ scripts/cliphist.sh
@@ -2,6 +2,9 @@
 
 case "$1" in
   d) cliphist list | rofi -dmenu -replace | cliphist delete ;;
-  w) cliphist wipe ;;
-  *) cliphist list | rofi -dmenu -replace -config ~/dotfiles/.config/rofi/config-cliphist.rasi  | cliphist decode | wl-copy ;;
+  w) if  [ `echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/config-short.rasi` == "Clear" ]; then
+      cliphist wipe
+      fi;;
+#  *) cliphist list | rofi -dmenu -replace -config ~/dotfiles/.config/rofi/config-cliphist.rasi  | cliphist decode | wl-copy
+  *) rofi -modi clipboard:~/.config/rofi/cliphist-rofi-img -show clipboard -show-icons  -config ~/dotfiles/.config/rofi/config-cliphist.rasi;;
 esac
